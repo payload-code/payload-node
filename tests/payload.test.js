afaa6c05722b86c799936b88ccc601d87059090c
Ian Halpern <ian@payload.co>
2023-07-25 16:09:33 -0400

    fixed spec for ClientToken

--- tests/payload.test.js
+++ tests/payload.test.js
@@ -9,7 +9,7 @@ import {
 } from './__fixtures__/accounts'
 
 describe('Test All Endpoints', () => {
-  const unselectable = ['APIKey', 'AccessToken']
+  const unselectable = ['APIKey', 'AccessToken', 'ClientToken']
   const selectable = Object.keys(objects).filter(
     (n) => !unselectable.includes(n),
   )
